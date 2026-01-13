3cb6b37d69a9777b4a6c034aac385705e80ce9d8
lobna8lobna <lobna89@gmail.com>
2026-01-13 19:09:03 +0100

    envoyer le fichier

--- probleme1.js
+++ probleme1.js
@@ -1,6 +1,10 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
 
+function awsname(prenom,nom) {
 
+    return (lobna ,maalej)
+
+}
 
 
 
@@ -10,7 +14,11 @@
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
 
+function somme(number1,number2){
 
+    return (number1+number2)/2
+
+}
 
 
 
@@ -20,7 +28,11 @@
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
 
+function string(){
 
+
+    return(bonjour,hello)
+}
 
 
 
@@ -31,7 +43,11 @@
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
 
 
+function stringheight(){
 
+
+    return (1,4)
+}
 
 
 
